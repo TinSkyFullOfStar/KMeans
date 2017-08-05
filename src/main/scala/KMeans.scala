464aa09d456d9bc4f2a1d1fd95d88ac3011fd854
TinSkyFullOfStar <867183036@qq.com>
2017-08-05 13:46:59 +0800

    complete getDist function

--- src/main/scala/KMeans.scala
+++ src/main/scala/KMeans.scala
@@ -1,6 +1,8 @@
 package main.scala
 
-import breeze.linalg.{DenseVector, Matrix}
+import breeze.linalg.{DenseVector, Matrix, sum}
+import breeze.numerics.{pow, sqrt}
+import sun.security.krb5.internal.crypto.Des
 
 import scala.io.{Source, StdIn}
 import scala.reflect.io.File
@@ -17,20 +19,6 @@ class KMeans {
 
   def kMeansAlgorithm(): Unit ={
     val vectorArr: Array[DenseVector[Double]] = getMatrix()
-    val indexs =getCoreIndex(vectorArr)
-  }
-
-
-  def getCoreIndex(arr: Array[DenseVector[Double]]): Array[Int] ={
-    if(pileCount != -1){
-      val range = Range(pileCount,arr.length-1,pileCount)
-      pileCount = -1
-      range.toArray
-    }else{
-      val range = Range(0,10)
-      range.toArray
-    }
-
   }
 
 
@@ -41,10 +29,43 @@ class KMeans {
     arrVector
   }
 
-  def getDis(): Double ={
-    val vector1 : DenseVector[Double] = DenseVector[Double]()
 
-    0.2
+  def initCore(arr: Array[DenseVector[Double]]): Array[DenseVector[Double]] ={
+    val range = Range(pileCount,arr.length-1,pileCount)
+    pileCount = -1
+    val coreArr = range.map(index => arr(index))
+    coreArr.toArray
+  }
+
+
+  def getCoreArr(arr: Array[DenseVector[Double]]) :Unit={
+
+  }
+
+
+  def getDis(indexVector: DenseVector[Double],coreVectors: Array[DenseVector[Double]]): ( DenseVector[Double], Int) ={
+    var dist: Double = 0.0
+    var tmp: Double = 0.0
+    var index: Int = 0
+    var count: Int = 0
+
+    val distVectors = coreVectors.map(_ - indexVector)
+
+    distVectors.foreach{ vector =>
+      count += 1
+      vector.foreach{ elem =>
+        tmp += math.pow(elem,2)
+      }
+
+      tmp = math.sqrt(tmp)
+      if(dist == 0 || dist > tmp){
+        dist = tmp
+        tmp = 0.0
+        index = count
+      }
+    }
+
+    Tuple2(indexVector,index)
   }
 
 
@@ -56,9 +77,23 @@ class KMeans {
 
 object KMeans{
   def main(args: Array[String]): Unit = {
-    val matrixStr = Source.fromFile("d:/KMeansDataSet/test_data2.txt").getLines()
-    val vectorArr = matrixStr.map(_.split("\\t")).map(vectorStr => vectorStr.map(_.toDouble))
-    val arrVector = vectorArr.map(arr => DenseVector(arr)).toArray
-    val range = Range(7,arrVector.length,7*2)
+//    val matrixStr = Source.fromFile("d:/KMeansDataSet/test_data2.txt").getLines()
+//    val vectorArr = matrixStr.map(_.split("\\t")).map(vectorStr => vectorStr.map(_.toDouble))
+//    val arrVector = vectorArr.map(arr => DenseVector(arr)).toArray
+//    val range = Range(7,arrVector.length,7*2)
+
+
+    val vector1 = DenseVector(0.5,3.3,0.4)
+    val vector2 = DenseVector(3.5,7.3,5.4)
+
+    val vector3 = vector1 + vector2
+    var sum: Double = 0.0
+    (vector1 - vector2).map{ x =>
+      val tmp = x * x
+      sum += tmp
+    }
+
+    val dist = sqrt(sum)
+    println(dist)
   }
 }
